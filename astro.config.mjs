8bc47e3f2d16de3534895b55bab4185e7d754d84
staubracing <staubracing@gmail.com>
2025-06-17 18:37:15 -0500

    un comment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://staubracing.github.io/staubracing-blog/",
-  // Comment out base for local development
-  // base: "/staubracing-blog/",
+  base: import.meta.env.PROD ? "/staubracing-blog/" : undefined,
   outDir: "./dist",
 });
