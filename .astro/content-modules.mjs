55aaf068209435bcce77fa9b5e6ad241b913fb01
staubracing <staubracing@gmail.com>
2025-10-15 18:48:11 -0500

    Remove obsolete Astro content files and schemas; delete unused content modules, data store, and type definitions to streamline project structure.

--- .astro/content-modules.mjs
+++
@@ -1,4 +0,0 @@
-
-export default new Map([
-["src/content/blog/racing/zx6r-engine-rebuild-part2.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fracing%2Fzx6r-engine-rebuild-part2.mdx&astroContentModuleFlag=true")]]);
-		
