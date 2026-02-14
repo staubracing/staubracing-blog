a54a93c908339d45d42634da1f23af33879d8cdd
staubracing <staubracing@gmail.com>
2026-02-14 16:07:08 -0600

    Update Astro configuration for site URL and base path; modify GitHub Actions workflow to deploy to AWS S3 and invalidate CloudFront cache instead of GitHub Pages.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,8 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 
 export default defineConfig({
-  site: "https://blog.staubracing.com",
+  site: "https://www.staubracing.com",
+  base: "/blog",
   outDir: "./dist",
   markdown: {
     remarkPlugins: [],
